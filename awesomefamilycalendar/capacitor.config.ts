0c5ef3be1b358fc33410f3262bfd655220d42417
Joshua Berrios <64435634+JoshuaB94@users.noreply.github.com>
2022-05-09 14:46:19 -0500

    Installed Capacitor.js + Setup & Configured + Fixed iOS Native Dependencies not updating.

---
+++ awesomefamilycalendar/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.awesomefamilycalendar.app',
+  appName: 'awesomefamilycalendar',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
